0025021629a4c312d765f783522dd2ea4a05eb25
ken088 <81868427+ken088@users.noreply.github.com>
2024-04-05 11:19:24 +0800

    Update py_白嫖.py

--- lib/py_白嫖.py
+++ lib/py_白嫖.py
@@ -47,7 +47,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 			result['filters'] = self.config['filter']	
 		return result
 	def homeVideoContent(self):
-		rsp = self.fetch("https://www.baipiaoys.com:9092")
+		rsp = self.fetch("https://baipiaoys.com:9092")
 		root = self.html(rsp.text)
 		aList = root.xpath("//div[@class='stui-vodlist__box']/a")
 
